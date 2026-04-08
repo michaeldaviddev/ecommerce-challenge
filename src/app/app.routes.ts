cbd081d19db0474cc8bded54263a4ff9a6b45546
Michael David Machacuay <mdavidmachacuay@icloud.com>
2026-04-08 11:26:06 -0500

    feat: add product detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'product/:id', loadComponent: () => import('./pages/product-detail/product-detail.page').then(c => c.ProductDetailPage) }
+];
---
+++ src/app/pages/product-detail/product-detail.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.page.html',
+  styleUrl: './product-detail.page.scss',
+})
+export class ProductDetailPage {}
