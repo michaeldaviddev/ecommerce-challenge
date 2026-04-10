537ff7bd76768b1bdee9669f953be99ef6a9152b
Michael David Machacuay <mdavidmachacuay@icloud.com>
2026-04-10 08:40:07 -0500

    feat: add vercel config

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { createRequestHandler } = await import('@angular/ssr/node');
   const { default: serverApp } = await import('../dist/ecommerce-challenge/server/server.mjs');
 
   const handlerFn = createRequestHandler(serverApp);
 
   return handlerFn(req, res);
-}
+};
